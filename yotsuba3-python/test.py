59f93bb4034bdd51de8fd64199019f9251c03bc9
Juti Noppornpitak <juti_n@yahoo.co.jp>
2010-02-05 16:01:59 +0000

    Added multi thread framework (work in progress)

--- yotsuba3-python/test.py
+++ yotsuba3-python/test.py
@@ -1,18 +1,57 @@
-from __future__ import print_function
-from yotsuba3.test import core_mt
+#from __future__ import print_function
+#from yotsuba3.test import core_mt
 
 import math
 
-print("Demonstration on passing functions as arguments")
+print "Demonstration on passing functions as arguments"
 
 def x(m, n):
-    print("%d" % sum([m, n]))
+    print "%d" % sum([m, n])
 
 def y(a, *b):
     a(*b)
 
+def z(*a):
+    if len(a) > 2:
+        a[0](*a[1:])
+    else:
+        a[0]()
+
+def p(a = None):
+    print a
+
+a = 20
+p(a = a)
 y(x, 1, 2)
+z(x, 1, 2)
 
-print("Now, the fun is over. Let's do the real stuff.")
+import time
+from Queue import Queue
+from threading import Thread
+import urllib2
 
-core_mt.run()
+st = time.time()
+
+def workerThread():
+    while True:
+        item = q.get()
+        f = urllib2.urlopen(item)
+        print "Done"
+        q.task_done()
+
+q = Queue()
+for i in range(4):
+     t = Thread(target=workerThread)
+     t.setDaemon(True)
+     t.start()
+
+for item in range(8):
+    q.put("http://c.shiroyuki.com/feeds/featured-content.xml")
+
+q.join()
+
+print "%s sec" % (time.time() - st)
+
+#print("Now, the fun is over. Let's do the real stuff.")
+
+#core_mt.run()
--- yotsuba3-python/yotsuba3/core/mt.py
+++ yotsuba3-python/yotsuba3/core/mt.py
@@ -11,23 +11,50 @@ current core is compatible with Python 2.6.
 import Queue
 import threading
 
-class MTBaseQueueException(Exception):
+version = 0.1
+
+class AgentThread(threading.Thread):
+    # Queue Reference
+    __dataQueue = None
+    
+    def __init__(self, threadObject, dataQueue = None):
+        if dataQueue is not None:
+            self.__dataQueue = dataQueue
+        else:
+            self.__dataQueue = Queue.Queue()
+    
+    def preprocess(self):
+        pass
+    
+    def run(self):
+        pass
+    
+    def postprocess(self):
+        pass
+    
+    pass
+
+class Manager(object):
+    __simulteneousThreadLimit = None
+    __agents = None
+    def __init__(self, simulteneousThreadLimit = None):
+        if simulteneousThreadLimit is not None:
+            self.__simulteneousThreadLimit = threading.BoundedSemaphore(simulteneousThreadLimit)
+        self.__agents = []
+    
+    def addThread(self, instanceOfAgentThread):
+        agent = instanceOfAgentThread
+        self.__agents.append(agent)
+    
+    def addFunction(self, referenceToFunction, *args, **kwargs):
+        worker = threading.Thread(target = referenceToFunction, args = args, kwargs = kwargs)
+        worker.setDaemon(True)
+        pass
+
+class BaseQueueException(Exception):
     def __init__(self, message = None):
         if message is None:
             message = "Generic multi-threading framework error exception"
         else:
             message = unicode(message)
         super(Exception, self).__init__(message)
-
-class MTBase(threading.Thread):
-    def lineUp(self, queue):
-        if 'queues' in dir(self):
-            self.queues = []
-        isAlreadyOnTheList = queue not in self.queues
-        if isAlreadyOnTheList:
-            self.queues.append(queue)
-        else:
-            raise MTBaseQueueException()
-    def init(self, function, args = None):
-        """Initialize the function to run"""
-        pass
