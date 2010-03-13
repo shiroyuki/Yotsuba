6655b3b8d7b208868dfbad7042904cfd0405623a
Juti Noppornpitak <juti_n@yahoo.co.jp>
2010-03-13 22:48:32 +0000

    Complete the multi-threading framework

--- yotsuba3-python/yotsuba3/core/mt.py
+++ yotsuba3-python/yotsuba3/core/mt.py
@@ -1,60 +1,128 @@
 """
-Multi-threading framework for Yotsuba 3
+High-level multi-threading framework for Yotsuba 3
 
 Author: Juti Noppornpitak <juti_n@yahoo.co.jp>
 Copyright: Shiroyuki Studio
 
 This framework is to make multi-threading programming in Python even easier. The
 current core is compatible with Python 2.6.
+
+At the current state of the development, there is a known limitation that it can
+only run one kind of the worker. Also, the framework will block the caller thread
+until all data is processed.
 """
 
-import Queue
-import threading
+import sys
+from Queue import Queue
+from threading import Thread, activeCount
 
-version = 0.1
+PACKAGE_VERSION = 1.0
 
-class AgentThread(threading.Thread):
-    # Queue Reference
-    __dataQueue = None
+def sampleWorkerFunction(data):
+    '''
+    This is the sample worker function/method.
     
-    def __init__(self, threadObject, dataQueue = None):
-        if dataQueue is not None:
-            self.__dataQueue = dataQueue
-        else:
-            self.__dataQueue = Queue.Queue()
-    
-    def preprocess(self):
-        pass
-    
-    def run(self):
-        pass
-    
-    def postprocess(self):
-        pass
-    
+    Requirements for the worker function/method:
+        1.  The first parameter must be something that stores the data from the queue.
+        2.  The worker never returns anything unless it is a new data that you want it
+            to be processed in the current pool.
+    '''
+    url = data
+    # do something with the data
+
+class ThreadTermSignal(object):
     pass
 
-class Manager(object):
-    __simulteneousThreadLimit = None
-    __agents = None
-    def __init__(self, simulteneousThreadLimit = None):
-        if simulteneousThreadLimit is not None:
-            self.__simulteneousThreadLimit = threading.BoundedSemaphore(simulteneousThreadLimit)
-        self.__agents = []
+class MultiThreadingFramework(object):
+    dataQueue = None
+    numberOfFalseTermSignal = None
     
-    def addThread(self, instanceOfAgentThread):
-        agent = instanceOfAgentThread
-        self.__agents.append(agent)
+    def run(self, input, worker, args = None, simulteneousWorkerLimit = None):
+        '''
+        Run multiple workers simulteneously (in multiple threads).
+        
+        The parameter *worker* is a reference to a function that will be called by
+        the thread.
+        
+        The parameter *input* is the input list that will be feeded to the queue.
+        
+        The parameter *worker* is the data processing function. there is no need for it to handle the synchronization.
+        
+        @param args: the arguement for the worker
+        @param simulteneousWorkerLimit: the limit of how many threads can be running simulteneously. (Optional, default: 4 or less depends on the length of urls)
+        '''
+        simulteneousWorkerLimit = (simulteneousWorkerLimit is None and type(simulteneousWorkerLimit) is not int) and 4 or simulteneousWorkerLimit
+        
+        self.dataQueue = Queue()
+        self.numberOfFalseTermSignal = 0
+        threads = []
+        
+        if type(input) is not list:
+                raise Exception("The list of inputs for this method must be a list.")
+        
+        if args is not None:
+            if type(args) not in [tuple, list]:
+                raise Exception("The arguements for this method must be either a tuple or a list.")
+            if len(args) > 0:
+                args = list(args)
+                args = [worker] + args
+                args = tuple(args)
+            else:
+                args = (worker, queue)
+        
+        # start threads
+        for i in range(simulteneousWorkerLimit):
+            t = Thread(target = __thread, args = args)
+            t.daemon = True
+            t.start()
+            threads.append(t)
+        
+        # add ThreadTermSignal
+        inputData = list(input)
+        inputData.extend([ThreadTermSignal] * simulteneousWorkerLimit)
+        
+        # put in the queue
+        for data in inputData:
+            self.__addData(data, False)
     
-    def addFunction(self, referenceToFunction, *args, **kwargs):
-        worker = threading.Thread(target = referenceToFunction, args = args, kwargs = kwargs)
-        worker.setDaemon(True)
-        pass
+        # block until all contents are downloaded
+        queue.join()
+        
+        del queue
+        for thread in threads:
+            del thread
+        del threads
+    
+    def __addData(self, data, addTermSignal):
+        self.dataQueue.put(data)
+        
+    def __thread(self, worker, *args):
+        try:
+            while True:
+                returnedData = None
+                data = self.dataQueue.get()
+                
+                # Check for the term signal
+                if data is ThreadTermSignal:
+                    # If this is false alarm
+                    if self.numberOfFalseTermSignal > 0:
+                        self.numberOfFalseTermSignal -= 1
+                        continue
+                    else:
+                        sys.exit()
+                
+                # Call the processor
+                if len(args) > 0:
+                    returnedData = worker(data, *args)
+                else:
+                    returnedData = worker(data)
+                
+                # If the worker return something, add the data to the queue.
+                if returnedData is not None:
+                    self.__addData(returnedData, True)
+                
+                queue.task_done()
+        except SystemExit:
+            queue.task_done()
+            pass
 
-class BaseQueueException(Exception):
-    def __init__(self, message = None):
-        if message is None:
-            message = "Generic multi-threading framework error exception"
-        else:
-            message = unicode(message)
-        super(Exception, self).__init__(message)
